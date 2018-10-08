154ff332240d297af07a8313a4e1ed06e6df9c01
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-08 18:14:10 +0200

    year random done

--- Project Meses Pareja/Program.cs
+++ Project Meses Pareja/Program.cs
@@ -10,12 +10,17 @@ namespace Project_Meses_Pareja
     {
         static void Main(string[] args)
         {
-            int month;
+            int month,randYear1,randYear2,year;
             Console.WriteLine("Escribe el mes");
             month = Int32.Parse(Console.ReadLine());
             CheckMonth(month);
+            Console.WriteLine("Escriba dos años entre los que generar uno aleatorio");
+            randYear1 = Int32.Parse(Console.ReadLine());
+            randYear2 = Int32.Parse(Console.ReadLine());
+            year = Year(randYear1, randYear2);
+            Console.WriteLine("El año generado es " + year);
 
-
+            
             Console.ReadKey();
         }
         public static int CheckMonth(int month)
@@ -32,5 +37,20 @@ namespace Project_Meses_Pareja
             } while (month < 1 || month > 12);
             return month;
         }
+        public static int Year(int randYear1,int randYear2)
+        {
+            int year;
+            Random random = new Random();
+            if (randYear2 > randYear1)
+            {
+                year = random.Next(randYear1, randYear2);//Max value al final  
+            }
+            else
+            {
+                year = random.Next(randYear2, randYear1);
+            }
+            return year;
+        }
+
     }
 }
