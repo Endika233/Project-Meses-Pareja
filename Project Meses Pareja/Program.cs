3078608b7a878017010c1528e7ce9d88893b4dee
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-08 17:32:35 +0200

    .

--- Project Meses Pareja/Program.cs
+++ Project Meses Pareja/Program.cs
@@ -10,6 +10,25 @@ namespace Project_Meses_Pareja
     {
         static void Main(string[] args)
         {
+            int month;
+            Console.WriteLine("Escribe el mes");
+            month = Int32.Parse(Console.ReadLine());
+            CheckMonth(month);
+
+        }
+        public static int CheckMonth(int month)
+        {
+            int count = 0;
+            do
+            {
+                if (count >0)
+                {
+                    Console.WriteLine("El número de mes no es correcto, introduzca de nuevo");
+                    month = Int32.Parse(Console.ReadLine());               
+                }
+                count = count + 1;
+            } while (month < 1 || month > 12);
+            return month;
         }
     }
 }
